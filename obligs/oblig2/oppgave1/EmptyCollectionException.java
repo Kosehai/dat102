208f5011b197e8a0f0a71eec7b38d5c955b6dde0
kamil <h596230@hvl.stud.no>
2022-02-21 10:01:42 +0100

    la til alt oppg1

---
+++ obligs/oblig2/oppgave1/EmptyCollectionException.java
@@ -0,0 +1,18 @@
+//********************************************************************
+//  EmptyCollectionException.java   //
+//  Representerersituasjonen når samlingen er tom.
+//********************************************************************
+
+public class EmptyCollectionException extends RuntimeException {
+
+  /**
+   *
+   */
+
+  /******************************************************************
+      Setter opp dette unntaket med passende melding.
+    ******************************************************************/
+  public EmptyCollectionException(String collection) {
+    super(" Denne " + collection + " er tom.");
+  }
+}
