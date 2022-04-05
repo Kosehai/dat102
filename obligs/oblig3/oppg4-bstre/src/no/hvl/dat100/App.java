ad52e996a33bdc88d61a054530e87e34786a2922
Martin <martin.wigum@gmail.com>
2022-04-05 20:59:29 +0200

    Ferdig med oppgave 4!

--- obligs/oblig3/oppg4-bstre/src/no/hvl/dat100/App.java
+++ obligs/oblig3/oppg4-bstre/src/no/hvl/dat100/App.java
@@ -2,8 +2,9 @@ package no.hvl.dat100;
 
 import java.util.Random;
 public class App {
-    static final int MAX_RNG = 5000;
+    static final int MAX_RNG = 20000;
     static final int NODER = 1023;
+    static final int NODER_TEST = 8191;
     static final int BS_MENGDE = 100;
     static Random rng = new Random();
     public static void main(String[] args) throws Exception {
@@ -32,7 +33,23 @@ public class App {
             BS_MENGDE, max_hoyde));
         System.out.println(String.format("vi) Gjennomsnittelig høyde av %d iterasjoner: %.3f", 
             BS_MENGDE, avg_hoyde));
-
+        System.out.println(String.format("\n\nc) Vi bruker gjennomsnittelige verdien for %d til å estimere gjennomsnittet til %d",
+            NODER, NODER_TEST));
+        System.out.println(String.format("Formel: %f = c*log2(%d)", avg_hoyde, NODER));
+        System.out.println(String.format("Vi snur på formelen: c = %f / log2(%d)", avg_hoyde, NODER));
+        double c = avg_hoyde / min_noder(NODER);
+        System.out.println(String.format("c = %f", c));
+        System.out.println(String.format("%f*log2(%d) = %f", c, NODER_TEST, c*(Math.log(NODER_TEST)/Math.log(2))));
+        System.out.println(String.format("Kjører test på %d iterasjoner", BS_MENGDE));
+        double avg_test = 0;
+        for(int i = 0; i < BS_MENGDE; i++){
+            BS_Tre<Integer> tre = new BS_Tre<>(0);
+            rngTre(NODER_TEST, tre);
+            Integer hoyde = tre.hoyde();
+            avg_test += hoyde;
+        }
+        avg_test = avg_test/BS_MENGDE;
+        System.out.println(String.format("Gjennomsnitts verdi for %d er %f", NODER_TEST, avg_test));
     }
 
     static void rngTre(int n, BS_Tre<Integer> tre){
